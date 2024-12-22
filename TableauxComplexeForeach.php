d80b89666714cb3095d40c12b7a26fa02ae434a1
Kinmaro212 <toti98741@gmail.com>
2024-12-22 15:39:11 +0100

    revision terminer en off

--- TableauxComplexeForeach.php
+++ TableauxComplexeForeach.php
@@ -1,7 +1,7 @@
 <?php
 $callOfDutyBlackOps6['Type'] = 'FPS';
 $callOfDutyBlackOps6['Console'] = 'Ps5';
-$callOfDutyBlackOps6['Public'] = '+18 ans';
+$callOfDutyBlackOps6['Public'] = '+ 18 ans';
 
 foreach($callOfDutyBlackOps6 as $clef => $valeur) {// ici la clef a une correspondance de valeur
     echo $clef.' a la valeur '.$valeur."<br>";
